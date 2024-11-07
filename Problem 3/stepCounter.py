5c80790511417f35440bf1f9cb37d3694407cbf2
willpgoode-student <wpg6239@utulsa.edu>
2024-11-07 11:33:05 -0600

    part of 3

--- Problem 3/stepCounter.py
+++ Problem 3/stepCounter.py
@@ -1,10 +1,7 @@
 def feet_to_steps(user_feet):
-   #write your code here
-   return
+   return (user_feet/2.5)
 
 if __name__ == '__main__':
-    #take input feet steps here
-    #store it into the function
-    
-    #print your steps here
+    user_feet = float(input())
+    print(f'{feet_to_steps(user_feet):.0f}')
     feet_to_steps(5280)
